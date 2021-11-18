dc4154f099dd7042737bcd12e3a9d4a5765ac6a6
ynhummel <yurihummel@outlook.com>
2021-11-18 11:23:55 -0300

    Main function expanded with some initial funcionalities and lib.rs created with the program logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use std::env;
+use std::process;
+
+use spgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else( |err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = spgrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
